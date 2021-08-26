63d38b8f8076acbb2661f3319e7bf7d7ea37ca32
Vilas Deshmukh <vilaspd@hotmail.com>
2021-08-26 12:45:04 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace vd204prepgitapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
